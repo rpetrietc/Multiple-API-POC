25f3aaf3fe54292e65828a8c05a7aa077673c34f
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2026-09-10 13:52:01 -0400

    Models to represent one API call

---
+++ Multiple-API-POC.Web/Models/ApiResultModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Multiple_API_POC.Web.Models;
+
+public class ApiResultModel
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Response { get; set; } = string.Empty;
+
+    public long ElapsedMilliseconds { get; set; }
+
+    public string Source { get; set; } = "API";
+}
---
+++ Multiple-API-POC.Web/Models/DemoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Multiple_API_POC.Web.Models;
+
+public class DemoViewModel
+{
+    public List<ApiResultModel> Results { get; set; } = [];
+
+    public long TotalElapsedMilliseconds { get; set; }
+
+    public string Mode { get; set; } = string.Empty;
+}
