b91f46e71d94b4b14e83d72bcfea5cd7e888ba2f
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2026-09-11 14:07:20 -0400

    Add simulated API failure handler for resilience demo

---
+++ Multiple-API-POC.Web/Services/SimulatedFailureHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace Multiple_API_POC.Web.Services;
+
+public class SimulatedFailureHandler : HttpMessageHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        HttpResponseMessage response = new(HttpStatusCode.ServiceUnavailable)
+        {
+            Content = new StringContent(
+                "Simulated API failure: Service Unavailable")
+        };
+
+        return Task.FromResult(response);
+    }
+}
